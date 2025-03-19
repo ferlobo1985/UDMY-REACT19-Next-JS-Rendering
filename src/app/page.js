e2332d621e9bd65ccd3542851d132b423b188812
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-19 13:22:13 -0300

    04-Home Employees
    04-Home Employees

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-export default function Home(){
+import axios from 'axios'
+import Link from 'next/link'
+
+export default async function Home(){
+  const employees = await axios.get('http://localhost:3004/employees')
+  const showEmployees = employees.data.map( employee =>(
+    <div key={employee.id} className='col'>
+      <div className='card'>
+          <div className='card-body'>
+            <h3>{employee.fullname}</h3>
+            <hr/>
+            <p className='card-text'>{employee.position}</p>
+            <p className='card-text'>{employee.age}</p>
+            <Link href={`/employee/${employee.id}`} className='btn btn-primary'>Go to empl</Link>
+          </div>
+      </div>
+    </div>
+  ))
+
   return(
     <>
       <h1>Employees:</h1>
+      <div className="row row-cols-1 rows-cols-sm-2 row-cols-md-3 g-3">
+        {showEmployees}
+      </div>
     </>
   )
 }
