5a9418d2e27faa98f374cd3e7e42cfe40df7d994
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-21 18:16:30 -0300

    32-PATCH employee
    32-PATCH employee

---
+++ src/app/api/update/[id]/route.js
@@ -0,0 +1,20 @@
+export async function PATCH(request,{ params }) {
+    const data = await request.json();
+    const ID =  (await params).id;
+
+    const res = await fetch(`http://localhost:3004/employees/${ID}`,{
+        method:'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+
+    if(!res.ok){
+        return Response.json(res.statusText,{
+            status:res.status
+        });
+    }
+
+    return Response.json('ok',{status:200})
+}
